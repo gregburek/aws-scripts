1597dd460c92c4e9488b069bdd908cfc8f4a23e7
Greg Burek <greg.burek@gmail.com>
2011-07-13 13:21:07 -0700

    Moved server startup stuff to user file

--- server-apds-user-data.sh
+++ server-apds-user-data.sh
@@ -24,44 +24,13 @@ aptitude -yq build-dep nginx
 
 gem install github-markup redcarpet
 
-mkdir /opt/run/
-mkdir /opt/log/
-mkdir /opt/log/nginx/
-mkdir /opt/lock/
-mkdir /opt/apps/
-mkdir /opt/html/
-mkdir /opt/source/
-cd /opt/source/
-
-wget http://projects.unbit.it/downloads/uwsgi-0.9.8.tar.gz
-tar -xzf uwsgi-*.tar.gz
-cd uwsgi-*
-make
-mv uwsgi /usr/local/lib/
-ln -s /usr/local/lib/uwsgi /usr/local/bin/uwsgi
-cd /opt/source/
-
-wget http://nginx.org/download/nginx-1.0.4.tar.gz
-tar -xzf nginx-*.tar.gz
-cd nginx-*
-touch /opt/run/nginx.pid
-touch /opt/lock/nginx.lock
-touch /opt/log/nginx/error.log
-./configure --conf-path=/etc/nginx/nginx.conf \
---error-log-path=/opt/log/nginx/error.log \
---pid-path=/opt/run/nginx.pid \
---lock-path=/opt/lock/nginx.lock \
---sbin-path=/usr/sbin \
---with-http_ssl_module
-
-make 
-make install
-wget -O /etc/init/nginx.conf http://wiki.nginx.org/index.php?title=Upstart&action=raw&anchor=nginx
-#chmod -R 777 /opt/
 
 useradd -m -p saB/M7hY0p7Bw gregburek -s /usr/bin/zsh
 usermod -a -G adm,admin gregburek
 echo "gregburek ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+mkdir /opt/
+chown gregburek:gregburek /opt
 
 su - gregburek -c "wget --no-check-certificate https://github.com/gregburek/aws-scripts/raw/master/dev.gregburek.com/env-gregburek.sh -O - | sh"
 
