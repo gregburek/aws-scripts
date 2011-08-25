13425eee5f7c504910c8d9e4fe4e3515fd99d000
Greg Burek <greg.burek@gmail.com>
2011-08-24 19:35:54 -0700

    Added comments and broke up long aptitude lines

--- server-apds-user-data.sh
+++ server-apds-user-data.sh
@@ -5,20 +5,22 @@
 #Purpose : Setup dev.apds.co env for my use
 #Comments :
 
+# Necessary for complete logging. Stock User-Data logs are capped and cut out early
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 set -e -x
 export DEBIAN_FRONTEND=noninteractive
 
+# Adds ec2-api-tool repo
 sudo apt-add-repository ppa:awstools-dev/awstools
-
 sudo aptitude -yq update && sudo aptitude -yq safe-upgrade
-
 sudo aptitude -yq install ec2-api-tools
 
-# recommended packages for ruby and rails for ruby and rails for ruby and rails for ruby and rails
-sudo aptitude -yq install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g
-sudo aptitude -yq install zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf
+# recommended packages for ruby and rails for ruby and rails
+sudo aptitude -yq install build-essential bison openssl libreadline6
+sudo aptitude -yq install libreadline6-dev curl git-core zlib1gz lib1g-dev
+sudo aptitude -yq install libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3
+sudo aptitude -yq install libxml2-dev libxslt-dev autoconf
 
 sudo aptitude -yq install exuberant-ctags git-core vim-nox zsh ack curl tmux
 sudo aptitude -yq install build-essential psmisc python-dev libxml2 libxml2-dev python-setuptools libssl-dev
